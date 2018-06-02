165ce1f15bf12d521167ae103b848ac4471d2f7f
Unknown <lucho.elp40@gmail.com>
2018-06-02 14:17:34 -0300

    Agregado evento para cerrar ventana

--- TP3Graficos-TomasMugetti/TP3Graficos-TomasMugetti/main.cpp
+++ TP3Graficos-TomasMugetti/TP3Graficos-TomasMugetti/main.cpp
@@ -10,6 +10,7 @@ const int SCREEN_H = 480;	//largo de pantalla
 
 int main(int argc, char **argv) {
 	ALLEGRO_DISPLAY *display = NULL;					// pantalla
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;			//cola de eventos
 	bool redraw = true;									// controla el refresco de pantalla
 	bool doexit = false;								//controla el loop de la ventana
 	/*inicializo allegro*/
@@ -25,12 +26,29 @@ int main(int argc, char **argv) {
 			NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return -1;
 	}
-	while (!doexit){								//mientras doexit no cambie la ventana no se cierra
-		if (redraw) {								//mientras redraw sea true la ventana se refresca
-			al_clear_to_color(al_map_rgb(0, 0, 0));	//se pone la pantalla en negro
-			al_flip_display();						//dibuja pantalla
+	/*inicializo cola de eventos*/
+	event_queue = al_create_event_queue();
+	if (!event_queue) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize event queue!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return -1;
+	}
+	al_register_event_source(event_queue, al_get_display_event_source(display)); //inicializo eventos de ventana
+	al_clear_to_color(al_map_rgb(0, 0, 0)); //pongo pantalla en negro
+	al_flip_display();						//dibuja pantalla
+	while (!doexit){			//windows loop
+		ALLEGRO_EVENT ev;		//variable para eventos
+		al_wait_for_event(event_queue, &ev);	//espero que haya eventos en la cola
+		if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {	//evento de cerrar ventana
+			return 0;
+			if (redraw && al_is_event_queue_empty(event_queue)) {
+				al_clear_to_color(al_map_rgb(0, 0, 0));	//se pone la pantalla en negro
+				al_flip_display();						//dibuja pantalla
+			}
 		}
 	}
 	al_destroy_display(display);					//destruye la pantalla
+	al_destroy_event_queue(event_queue);			//destruye cola de eventos
 	return 0;
 }
